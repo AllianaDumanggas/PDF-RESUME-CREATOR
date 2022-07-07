411f111d1861e29c0a117a3d81a432ed45bcc112
unknown <urceline0130@gmail.com>
2022-07-07 21:44:46 +0800

    Can read the json file and display it into the textbox

--- Form1.cs
+++ Form1.cs
@@ -94,7 +94,51 @@ namespace PDF_RESUME_CREATOR
 
         private void readjdonfilebttn_Click(object sender, EventArgs e)
         {
-
+            Info mydata = new Info
+            {
+                CompleteName = "Marites Chizmoza",
+                Age = 25,
+                Birthday = "July 27 1995",
+                Address = "123 Anywhere St, New YOrk City",
+                Profile = "A responsible and committed individual who has the ability to function and be flexible in a team environment and has a strong leadership and time management skills.Strong analytical and creative skills with an eye for details.Passionate about working in a multi - cultural environment, as that involves creation, growth and experience in my field of profession.",
+                Contact = new List<string>()
+                {
+                    "Phone Number: 09123456789",
+                    "Email: MaritesChizmoza@gmail.com"
+                },
+                Skills = new List<string>()
+                {
+                    "Technology Skills",
+                    "Computer Skills",
+                    "Creative Design",
+                    "Creative Problem-Solving Skills",
+                    "Multitasking",
+                    "Time Management",
+                    "Customer Service",
+                    "Efficiency",
+                    "Self-Discipline",
+                    "Perseverance",
+                },
+                Education = new List<string>()
+                {
+                    "Margareth Science School: STEM (2010-1012)",
+                    "Harvard University: Bachelor of Science in Mass Communication(2012-2018)",
+                },
+                Experience = new List<string>()
+                {
+                    "Administrative Secretary at Grand Cali, San Francsico (2019-2022)",
+                    "Data Manager at Monaco Inc, Real California (2022-2025)",
+                    "Executive Data Analyst at Casanix, Toronto, Canada (2025-present)",
+                },
+                References = new List<string>()
+                {
+                    "Maria Chismis (Analyst Director at Issuan Company in Mexico",
+                    "Juan Balita (Software Engineer at Techwix in New York)",
+                    "Jose News (Resaturant and Hotel Manger at Micasa in Usa)",
+                }
+            };
+            string strresultsjson = JsonConvert.SerializeObject(mydata);
+            datatxtbox.Text = strresultsjson;
         }
     }
 
